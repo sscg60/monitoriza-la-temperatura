c1a1c64f8edc5dcdc4910bf68fd31c67c5d018fb
sscg60 <sscg60@educastillalamancha.es>
2025-12-03 22:17:15 +0100

    Actualizar pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,4 +2,9 @@ let TEMPERATURA = 0
 input.onGesture(Gesture.Shake, function () {
     TEMPERATURA = TEMPERATURA
     basic.showNumber(input.temperature())
+    if (TEMPERATURA > 21) {
+        pins.digitalWritePin(DigitalPin.P0, 1)
+    } else {
+        pins.digitalWritePin(DigitalPin.P0, 0)
+    }
 })
